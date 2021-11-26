8e32146e15401c1184f832079df7243b24f04a6b
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2021-11-26 16:22:56 +0000

    End of wk1

---
+++ Fibonacci/src/main/java/com/sparta/fib/Tester.java
@@ -0,0 +1,15 @@
+package com.sparta.fib;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Tester {
+    public static void main(String[] args) {
+        List<String> myList = new Stack<>();
+        myList.add("Ed");
+        myList.add("Jakub");
+        System.out.println(myList);
+    }
+}
