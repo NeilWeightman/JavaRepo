acd3e45396fc6ef118fec65cbb336714bdb869e3
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2021-12-08 13:43:33 +0000

    After lunch

--- JDBCExample/src/main/java/com/sparta/jdbc/JDBCDriver.java
+++ JDBCExample/src/main/java/com/sparta/jdbc/JDBCDriver.java
@@ -2,7 +2,9 @@ package com.sparta.jdbc;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCDriver {
     public static void main(String[] args) {
@@ -13,5 +15,15 @@ public class JDBCDriver {
             e.printStackTrace();
         }
         System.out.println("Connection object: " + conn);
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM actor");
+            while (result.next()) {
+                System.out.println("Name: " + result.getString("first_name") + " " +
+                        result.getString(3));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
