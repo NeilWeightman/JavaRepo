ca12f475b1d3b1277ea1239b0b2b1b8c91dbfd90
Neil Weightman <82580266+NeilWeightman@users.noreply.github.com>
2021-12-16 15:27:19 +0000

    End of Thursday week 4

--- HibernateDemo/src/main/java/com/sparta/hibernatedemo/controllers/ActorController.java
+++ HibernateDemo/src/main/java/com/sparta/hibernatedemo/controllers/ActorController.java
@@ -1,8 +1,11 @@
 package com.sparta.hibernatedemo.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.hibernatedemo.entities.Actor;
 import com.sparta.hibernatedemo.repositories.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,17 +17,31 @@ import java.util.Optional;
 public class ActorController {
     @Autowired
     private ActorRepository theRepository;
+    @Autowired
+    ObjectMapper map;
     @GetMapping(value="/sakila/actors")
     public List<Actor> getActors(){
         return theRepository.findAll();
     }
     @GetMapping(value="/sakila/actor")
-    public Actor getActor(@RequestParam Integer id){
+    public ResponseEntity<String> getActor(@RequestParam Integer id){
         Optional<Actor> result = theRepository.findById(id);
-        if(result.isPresent())
-            return result.get();
-        else
-            return null;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("content-type", "application/json");
+        if(result.isPresent()) {
+            try {
+                ResponseEntity<String> resp = new ResponseEntity<>(map.writeValueAsString(result.get()),
+                        headers,
+                        HttpStatus.OK);
+                return resp;
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+//            return result.get();
+        }
+        return new ResponseEntity<String>("{\"message\":\"That actor doesn't exist\"}",
+                    headers,
+                    HttpStatus.OK);
     }
     @PutMapping(value="/sakila/actor/update")
     public Actor updateActor(@RequestBody Actor newState){
